45fa441b146865d2c6d1fa14ae8553c1d482c082
ttwiz_z <moderkascripts@gmail.com>
2024-04-15 17:35:35 +0300

    Version 1.2-1

--- src/pixcrypt.lua
+++ src/pixcrypt.lua
@@ -9,7 +9,7 @@
 ]]
 
 
-local pixcrypt = {}
+local pixcrypt = { Secret = 2773480762 }
 
 
 function pixcrypt:Encrypt(Key, String)
@@ -17,30 +17,35 @@ function pixcrypt:Encrypt(Key, String)
     assert(type(String) == "string", "Argument 2 (String) must be a string")
 
     local Summand = #Key + math.pi * 4.3579
+    local Index = 1
     local Result = ""
 
     for Character in string.gmatch(Key, ".") do
         Summand = Summand + string.byte(Character) / 1.8602
     end
     for Character in string.gmatch(String, ".") do
-        Result = Result .. string.byte(Character) / 2.1748 + Summand .. "xip"
+        Result = Result .. string.byte(Character) / 2.1748 + Summand * (pixcrypt.Secret / Index) .. "xip"
+        Index = Index + 1
     end
 
     return string.gsub(string.reverse(string.sub(Result, 1, -4)), "%.", "$")
 end
+
 
 function pixcrypt:Decrypt(Key, String)
     assert(type(Key) == "string", "Argument 1 (Key) must be a string")
     assert(type(String) == "string", "Argument 2 (String) must be a string")
 
     local Summand = #Key + math.pi * 4.3579
+    local Index = 1
     local Result = ""
 
     for Character in string.gmatch(Key, ".") do
         Summand = Summand + string.byte(Character) / 1.8602
     end
     for Character in string.gmatch(string.gsub(string.reverse(String), "%$", "."), "([^xip]+)") do
-        Result = Result .. string.char(math.floor((Character - Summand) * 2.1748 + 0.5))
+        Result = Result .. string.char(math.floor((Character - Summand * (pixcrypt.Secret / Index)) * 2.1748 + 0.5))
+        Index = Index + 1
     end
 
     return Result
